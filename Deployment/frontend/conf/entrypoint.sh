6488b8a6b81fa1f30ded0e0aa8318dc8bdbd9a83
Tal Hason <97454240+tal-hason@users.noreply.github.com>
2023-04-02 14:16:24 +0000

    new version

--- Deployment/frontend/conf/entrypoint.sh
+++ Deployment/frontend/conf/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 echo "update ngnix config"
-envsubst '\${HOST1} \${HOST2} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.conf
+envsubst '\${HOST1} \${HOST2} \${HOST3} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.conf
 exec "$@"
